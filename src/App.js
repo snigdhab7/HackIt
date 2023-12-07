95059f2b776193a66b38ae06fc77cd9967cc76a9
Harika Bale <saritha.ind54@gmail.com>
2023-12-06 22:10:44 -0500

    added profile screen UI and Style

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import LoginHome from './Login';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignUpUser from './Login/SignUpUser/SignUpUser';
+import Profile from './Profile/Profile';
 function App() {
   return (
     <div className="App">
@@ -15,6 +16,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginHome />} />
         <Route path="/signup" element={<SignUpUser />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </Router>
     </div>
