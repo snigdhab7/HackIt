4db9c4f6ac9deb8f4a5428df197ee77e1c7ffdfc
nethraarul20 <arulmozhi.k@northeastern.edu>
2023-12-10 01:12:03 -0500

    changes

--- src/EventDetails/EventDetails.js
+++ src/EventDetails/EventDetails.js
@@ -4,13 +4,16 @@ import React from "react";
 import "./EventDetails.css";
 import { Link } from "react-router-dom";
 import StarRating from "./StarRating";
-import * as client from "./client";
+import * as client from "../Dashboard/client";
 import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { format } from "date-fns";
+
 function EventDetails() {
   const [isModalOpen, setModalOpen] = useState(false);
   const [showConfirmation, setShowConfirmation] = useState(false);
   const [events, setEvents] = useState([]);
-
+  const { eventId } = useParams();
   
   const openModal = () => {
     setModalOpen(true);
@@ -21,8 +24,6 @@ function EventDetails() {
     setShowConfirmation(false);
   };
 
-
-
   const handleRegisterClick = () => {
       closeModal();
       return <Link to="/Dashboard" replace="true" />;
@@ -32,21 +33,39 @@ function EventDetails() {
     setShowConfirmation(true);
   };
 
-  const fetchEvents = async () => {
-    const events = await client.findAllEvents();
-    setEvents(events);
+  const formatDate = (dateString) => {
+    try {
+      const date = new Date(dateString);
+      // Check if the date is valid
+      if (isNaN(date.getTime())) {
+        console.log("Invalid date");
+      }
+      return format(date, "do MMMM yyyy");
+    } catch (error) {
+      console.error('Error parsing date:', error);
+      return 'Invalid date';
+    }
   };
 
   useEffect(() => {
-    fetchEvents();
-  }, []);
+    const fetchEventDetails = async () => {
+      try {
+        const eventData = await client.findEventById(eventId);
+        setEvents(eventData); // 
+      } catch (error) {
+        console.error("Error fetching event details:", error);
+      }
+    };
+ 
+    fetchEventDetails();
+  }, [eventId]);
 
   return (
     <div>
       <header className="header-background">
       </header>
       <div className="event-details">
-     {events.map((event)=> (
+      {events && (
        <div>
       <div className="float-end">
           <button className="register-button me-2" onClick={handleDeregisterClick}>Deregister</button>
@@ -54,22 +73,22 @@ function EventDetails() {
         </div>
         <div className="event-heading">
           <StarRating/>
-          <h2 class="event-heading-color">{event.eventName}
+          <h2 class="event-heading-color">{events.eventName}
           </h2>
-          <p className="medium-text">Join us for an in-person Data and Tech Workshop where small and medium business leaders can enhance their skills and stay ahead of the game</p>
+          <p className="medium-text">{events.summary}</p>
         </div>
         <div className="event-heading">
          <h5 class="event-heading-color">Date and time</h5>
         <p className="medium-text">
         <FaCalendar className="me-3 color-palette"/>
-          {event.date}
+        {formatDate(events.date)}
           </p>
         </div>
         <div className="event-heading">
          <h5 class="event-heading-color">Location</h5>
          <p className="medium-text">
           <FaLocationArrow className="me-3 color-palette"/>
-          {event.venue}
+          {events.venue}
           </p>
         </div>
         <div className="event-heading">
@@ -77,13 +96,13 @@ function EventDetails() {
          <p className="medium-text">
           <p className="medium-text">
           <FaClock className="color-palette me-2"/>
-         {event.timeStart}
+         {events.timeStart}
           </p>
-         {event.description}
+         {events.description}
          </p>
         </div>
         </div>
-))}
+      )}
       </div>
 
       <footer>
