a3a3bb2284d844feaa2fa47a39b3e7a24353699e
Harika Bale <saritha.ind54@gmail.com>
2023-12-11 19:54:40 -0500

    fetched all details

--- src/ExternalApi/Events.js
+++ src/ExternalApi/Events.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import * as client from './Client';
 
 function Events() {
-    
+
     const [events, setEvents] = useState([]);
     useEffect(() => {
         fetchEvents();
@@ -20,14 +21,24 @@ function Events() {
             <div>
                 {events.map((event, index) => (
 
-                    <div key={index}>
-                        <h2>{event.title}</h2>
-                        <p>Link: <a href={event.link}>{event.link}</a></p>
-                    </div>
+                    <Link to={event.link}>
+
+                        <div key={index} className="event-card" style={{ margin: "10px" }}>
+                            <img src={event.image} alt={event.title} />
+                            <div>
+                                <h2>{event.title}</h2>
+                                <p>{event.description}</p>
+                                <p>Duration: {event.date.when}</p>
+                                <p>{event.address}</p>
+                            </div>
+                        </div>
+
+                    </Link>
 
                 ))}
-            </div>            
-           
+            </div>
+
+
         </div>
     );
 
