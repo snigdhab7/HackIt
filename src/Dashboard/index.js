323f715347c517e7e8b179f920aa8bd69a7d7a74
Snigdha Bose <62890614+snigdhab7@users.noreply.github.com>
2023-12-13 14:53:43 -0500

    dashboard

--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -63,8 +63,56 @@ const Dashboard = () => {
 
           {/* Slider Carousel */}
           <Slider {...carouselSettings}>
-            {/* Your existing carousel slides */}
-          </Slider>
+          <div>
+            <img
+              src="https://www.aami.org/images/default-source/htm/htm-week/htm24emailheader.jpg?sfvrsn=aebdf854_3"
+              alt="Event Management 1"
+              className="display-photo1"
+            />
+          </div>
+          <div>
+            {/* Add more images for the carousel */}
+            <img
+              src="https://creative-europe-desk.de/files/styles/slider_1400x735_exakt/public/images/vrdayseurope_immersive_tech.jpg"
+              alt="Event Management 2"
+              className="display-photo1"
+            />
+          </div>
+          <div>
+            {/* Add more images for the carousel */}
+            <img
+              src="https://theblackwomenintech.com/wp-content/uploads/2022/12/July-Events-Eventbrite-Title-Template-4.jpg"
+              alt="Event Management 2"
+              className="display-photo1"
+            />
+          </div>
+          <div>
+            {/* Add more images for the carousel */}
+            <img
+              src="https://eventbrowse.com/wp-content/uploads/2023/07/643489949a8e96bb996e787a_Tech20Week20Meta-JsVY83.tmp_.jpg"
+              alt="Event Management 2"
+              className="display-photo1"
+            />
+          </div>
+          <div>
+            {/* Add more images for the carousel */}
+            <img
+              src="https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F563913089%2F496415951365%2F1%2Foriginal.20230728-093246?h=230&w=460&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C241%2C1080%2C540&s=8de3ab39266095120b7ad8f334bd9eb3"
+              alt="Event Management 2"
+              className="display-photo1"
+            />
+          </div>
+          <div>
+            {/* Add more images for the carousel */}
+            <img
+              src="https://saeindia.org/jbframework/uploads/2023/03/Banner-2023-03-23T184459.521-1365x384.jpg"
+              alt="Event Management 2"
+              className="display-photo1"
+            />
+          </div>
+          {/* Add more slides as needed */}
+        </Slider>
+          <br />
           <br />
 
           {/* Search Bar */}
